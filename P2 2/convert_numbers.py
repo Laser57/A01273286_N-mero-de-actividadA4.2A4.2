a725bd4a82564f9a4ac39d85a945f6d61dc569e1
Luis Alfonso <lasesabanero123@hotmail.com>
2024-02-04 19:30:33 -0600

    actualizamos pep8

--- P2 2/convert_numbers.py
+++ P2 2/convert_numbers.py
@@ -10,9 +10,9 @@ ENERO 2024
 '''
 import time
 import sys
-TIEMPO_INICIO = time.time()
-MASTER = []
-NUMEROS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '.', ',']
+tiempo_inicio = time.time()
+master = []
+numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '.', ',']
 RESULTADO_ARCHIVO = "ConvertionResults.txt"
 NOMBRE_ARCHIVO = sys.argv[1]
 #LEEMOS ARCHIVO, NOS ASEGURAMOS QUE LOS VALORES NUMERICOS SEAN VALIDOS
@@ -21,28 +21,28 @@ with open(NOMBRE_ARCHIVO, 'r', encoding='UTF-8') as file:
     for linea in file:
         AUX = ""
         for i in linea.strip():
-            if i in NUMEROS:
+            if i in numeros:
                 AUX += i
             else:
                 break
         if AUX != "":
-            MASTER.append(int(AUX))
+            master.append(int(AUX))
         else:
             print(f"Linea {linea} con valor invalido, se omite")
 #PARTE BINARIA, SE GUARDA EL RESULTADO EN UNA LISTA
-LISTA_BINARIA=[]
-for NUMERO in MASTER:
+lista_binaria=[]
+for numero in master:
     BINARIO=""
     NEGATIVO=False
 #BANDERA NEGATIVA, EN CASO DE SER NEGATIVO PASAMOS VALOR ABSOLUTO
-    if NUMERO<0:
+    if numero<0:
         NEGATIVO=True
-        NUMERO=abs(NUMERO)
+        numero=abs(numero)
 #USAREMOS EL RESIDUO DE LA OPERACION MODULO % LA BASE NUMERICO
-    while NUMERO > 0:
-        RESIDUO = NUMERO % 2
-        BINARIO = str(RESIDUO) + BINARIO
-        NUMERO //= 2
+    while numero > 0:
+        residuo = numero % 2
+        BINARIO = str(residuo) + BINARIO
+        numero //= 2
 #VALOR NEGATIVO SE USARA 8 BITS PARA REPRESENTAR EL NUMERO, SE USARA EL INVERSO DEL
 #NUMERO POSITIVO Y SE LE SUMARA UN COMPLEMENTO + 1
     if NEGATIVO:
@@ -53,39 +53,39 @@ for NUMERO in MASTER:
         ACARREO = 0
 #USAMOS 8 BITS PARA EL CICLO, USAMOS UNA SUMA BINARIA PARA APLICAR EL +1
         for i in range(8 - 1, -1, -1):
-            BIT1 = int(BINARIO[i])
-            BIT2 = int(COMPLEMENTO[i])
-            SUMA_BIT = BIT1 + BIT2 + ACARREO
-            RESULTADO = str(SUMA_BIT % 2) + RESULTADO
-            ACARREO = SUMA_BIT // 2
+            bit1 = int(BINARIO[i])
+            bit2 = int(COMPLEMENTO[i])
+            suma_bit = bit1 + bit2 + ACARREO
+            RESULTADO = str(suma_bit % 2) + RESULTADO
+            ACARREO = suma_bit // 2
         if ACARREO:
             RESULTADO = "1" + RESULTADO
-        LISTA_BINARIA.append(RESULTADO)
+        lista_binaria.append(RESULTADO)
     else:
-        LISTA_BINARIA.append(BINARIO)
+        lista_binaria.append(BINARIO)
 #PARTE HEXADECIMAL USAMOS OPERACION MODULO %16
-LISTA_HEXADECIMAL=[]
-for NUMERO in MASTER:
+lista_hexadecimal=[]
+for numero in master:
     HEX=""
-    if NUMERO < 0:
-        NUMERO = 2**32 + NUMERO
-    while NUMERO > 0:
-        RESIDUO = NUMERO % 16
+    if numero < 0:
+        numero = 2**32 + numero
+    while numero > 0:
+        residuo = numero % 16
 #APLICAMOS TRANSFORMACION DE NUMERO PARA VALORES MAYORES A 16 A,B,C,D,E,F
-        if RESIDUO>=10:
-            RESIDUO=chr(ord('A') + RESIDUO - 10)
-        HEX = str(RESIDUO) + HEX
-        NUMERO //= 16
-    LISTA_HEXADECIMAL.append(HEX)
+        if residuo>=10:
+            residuo=chr(ord('A') + residuo - 10)
+        HEX = str(residuo) + HEX
+        numero //= 16
+    lista_hexadecimal.append(HEX)
 ##CALCULAMOS TIEMPO FINAL
-TIEMPO_FINAL = time.time() - TIEMPO_INICIO
+tiempo_final = time.time() - tiempo_inicio
 #VACIAMOS EL LAS LISTAS HEXADECIMAL Y BINARIA PARA GUARDAR TXT
 with open(RESULTADO_ARCHIVO, 'a',encoding='UTF-8') as ARCHIVO:
     ARCHIVO.write(f"Archivo: {sys.argv[1]}\n")
-    for indice, numero in enumerate(MASTER):
-        binario = LISTA_BINARIA[indice]
-        hexadecimal = LISTA_HEXADECIMAL[indice]
+    for indice, numero in enumerate(master):
+        binario = lista_binaria[indice]
+        hexadecimal = lista_hexadecimal[indice]
         ARCHIVO.write(f"DECIMAL: {numero} BINARIO: {binario} HEXADECIMAL: {hexadecimal}\n")
         print(f"DECIMAL: {numero} BINARIO: {binario} HEXADECIMAL: {hexadecimal}")
-    ARCHIVO.write(f"Tiempo de ejecución:{TIEMPO_FINAL}\n")
-print(f"Tiempo de ejecución: {TIEMPO_FINAL} segundos")
+    ARCHIVO.write(f"Tiempo de ejecución:{tiempo_final}\n")
+print(f"Tiempo de ejecución: {tiempo_final} segundos")
